d29f7b5ff7fda36cb83ad0a0fa720f7bfc6964b2
Blake <blakeedwincalkins@gmail.com>
2024-02-29 15:17:29 -0700

    fix entries.js creates obj now

--- startup/entries.js
+++ startup/entries.js
@@ -1,38 +1,40 @@
 function makeEntryObj() {
+    // event.preventDefault();
     let restaurant = document.getElementById('restaurant').value;
     let date = document.getElementById('date').value;
     let dish = document.getElementById('dish').value;
     let blurb = document.getElementById('blurb').value;
     // let rating = document.querySelector('rating');
     let thoughts = document.getElementById('thoughts').value;
-    console.log("vars declared");
 
-    if (restaurant.trim() === '' || 
-    date.trim() === '' || 
-    blurb.trim() === '' || 
-    dish.trim() === '' ||
-    thoughts.trim() === '') {
-        alert('Please enter a value before submitting.');
-        return false;
-      }
-      console.log("if passed");
 
-//     var ratingButtons = document.querySelectorAll('input[name="rating"]');
+    // if (restaurant.trim() === '' || 
+    // date.trim() === '' || 
+    // blurb.trim() === '' || 
+    // dish.trim() === '' ||
+    // thoughts.trim() === '') {
+    //     alert('Please enter a value before submitting.');
+    //     return false;
+    //   }
 
-//   // Add a change event listener to the radio buttons
-//     ratingButtons.forEach(function(button) {
-//         // Get the value of the selected radio button
-//         var selectedRating = document.querySelector('input[name="rating"]:checked').value;
-//     });
-    console.log('before obj creation');
-    // const entryObj = {
-    //     'restaurant': restaurant,
-    //     'date': date,
-    //     'dish': dish,
-    //     'blurb': blurb,
-    //     // 'rating': selectedRating,
-    //     'thoughts': thoughts,
-    // };
-    // console.log(entryObj);
-    console.log("pain");
+    var ratingButtons = document.querySelectorAll('input[name="rating"]');
+    var selectedRating = 0;
+
+  // Add a change event listener to the radio buttons
+    ratingButtons.forEach(function(radio) {
+        // Get the value of the selected radio button
+        if (radio.checked) {
+            selectedRating = radio.value;
+        }
+    });
+    const entryObj = {
+        'restaurant': restaurant,
+        'date': date,
+        'dish': dish,
+        'blurb': blurb,
+        'rating': selectedRating,
+        'thoughts': thoughts,
+    };
+    const entryObjString = JSON.stringify(entryObj);
+    localStorage.setItem("")
 }
