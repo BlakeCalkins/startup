8029254d15100a07f7ec1556651e729e87a12cd6
Blake <blakeedwincalkins@gmail.com>
2024-02-28 21:12:39 -0700

    init entries.js

---
+++ startup/entries.js
@@ -0,0 +1,6 @@
+function makeEntryObj() {
+    
+    const entryObj = {
+        
+    }
+}
--- startup/login.js
+++ startup/login.js
@@ -1,6 +1,11 @@
 function login() {
     const nameEl = document.querySelector("#name");
     const pswd = document.querySelector("#password");
+    if (nameEl.trim() === '' || pswd.trim() === '') {
+        alert('Please enter a value before submitting.');
+        return false;
+      }
+      
     localStorage.setItem("userName", nameEl.value);
     localStorage.setItem("password", pswd.value);
     window.location.href = "homepage.html";
