9da2d8a225629e05ecd502c65d13dc46e295c1b9
Blake <blakeedwincalkins@gmail.com>
2024-03-01 20:04:26 -0700

    add functionality to remove friend button

--- startup/friends.js
+++ startup/friends.js
@@ -8,9 +8,12 @@ function retrieveSet() {
     return retrievedSet;
 }
 
-function storeFriend(friendSet, friend) {
+function storeSet(friendSet) {
     let friendArray = Array.from(friendSet);
     localStorage.setItem('friendKey', JSON.stringify(friendArray));
+}
+function storeFriend(friendSet, friend) {
+    storeSet(friendSet);
     num = localStorage.getItem("friendNumber") ?? 0;
     num++;
     currFriend = 'friend' + num;
@@ -49,19 +52,55 @@ function renderFriend(friend) {
     let newFriend = document.createElement('li');
     newFriend.textContent = friend;
     newFriend.className = 'entry';
+    newFriend.id = "li" + friend;
 
     let newHomepageButton = document.createElement('button');
     newHomepageButton.type = 'button';
     newHomepageButton.textContent = "View Home Page";
     newHomepageButton.className = "btn btn-outline-primary";
+    newHomepageButton.id = friend + "Home";
     
     let newRemoveButton = document.createElement('button');
     newRemoveButton.type = 'button';
     newRemoveButton.textContent = "Remove Friend";
     newRemoveButton.className = "btn btn-outline-danger";
+    newRemoveButton.id = friend + "Remove";
+    newRemoveButton.onclick = function () {
+        removeFriend(friend);
+    }
+
 
     let existingList = document.getElementById('friendList');
     existingList.appendChild(newFriend);
     existingList.appendChild(newHomepageButton);
     existingList.appendChild(newRemoveButton);
+}
+
+function removeFriend(friend) {
+    // Find friend in localStorage using the value instead of the key,
+    // then remove that friend
+    for (let i = 0; i < localStorage.length; i++) {
+        const key = localStorage.key(i);
+        const itemValue = localStorage.getItem(key);
+
+        if (itemValue === friend) {
+            localStorage.removeItem(key);
+        }
+    }
+    // Remove friend from the set of friends stored in localStorage
+    let friendSet = retrieveSet();
+    friendSet.delete(friend);
+    storeSet(friendSet);
+    // Decrement the friend count in localStorage
+    num = localStorage.getItem("friendNumber");
+    num--;
+    localStorage.setItem("friendNumber", num);
+
+    // Remove the HTML elements
+    const liFriendDelete = document.getElementById("li" + friend);
+    const friendHome = document.getElementById(friend + "Home");
+    const friendRemove = document.getElementById(friend + "Remove");
+    liFriendDelete.remove();
+    friendHome.remove();
+    friendRemove.remove();
 }
